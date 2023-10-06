7723ab03b3cd84f1c57131c21f8d32349f4eb6d9
Deryko <143024690+Deryko@users.noreply.github.com>
2023-10-06 17:00:30 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -31,7 +31,6 @@ dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
 if [[ "$hst" != "$dart" ]]; then
 echo "$localip $(hostname)" >> /etc/hosts
 fi
-
 mkdir -p /etc/xray
 mkdir -p /etc/v2ray
 touch /etc/xray/domain
@@ -115,14 +114,43 @@ echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 
+mkdir -p /etc/cobek
+mkdir -p /etc/cobek/limit
+mkdir -p /etc/cobek/limit/trojan
+mkdir -p /etc/cobek/limit/vless
+mkdir -p /etc/cobek/limit/vmess
+mkdir -p /etc/cobek/limit/ssh
+mkdir -p /etc/cobek/limit/ssh/ip
+mkdir -p /etc/cobek/limit/trojan/ip
+mkdir -p /etc/cobek/limit/trojan/quota
+mkdir -p /etc/cobek/limit/vless/ip
+mkdir -p /etc/cobek/limit/vless/quota
+mkdir -p /etc/cobek/limit/vmess/ip
+mkdir -p /etc/cobek/limit/vmess/quota
+mkdir -p /etc/cobek/trojan
+mkdir -p /etc/cobek/vless
+mkdir -p /etc/cobek/vmess
+mkdir -p /etc/cobek/log
+mkdir -p /etc/cobek/log/trojan
+mkdir -p /etc/cobek/log/vless
+mkdir -p /etc/cobek/log/vmess
+mkdir -p /etc/cobek/log/ssh
+mkdir -p /etc/cobek/cache
+mkdir -p /etc/cobek/cache/trojan-tcp
+mkdir -p /etc/cobek/cache/trojan-ws
+mkdir -p /etc/cobek/cache/trojan-grpc
+mkdir -p /etc/cobek/cache/vless-ws
+mkdir -p /etc/cobek/cache/vless-grpc
+mkdir -p /etc/cobek/cache/vmess-ws
+mkdir -p /etc/cobek/cache/vmess-grpc
+mkdir -p /etc/cobek/cache/vmess-ws-orbit
+mkdir -p /etc/cobek/cache/vmess-ws-orbit1
 mkdir -p /var/lib/SIJA >/dev/null 2>&1
 echo "IP=" >> /var/lib/SIJA/ipvps.conf
 
 echo ""
-wget -q https://raw.githubusercontent.com/artanodrop/v4/main/tools.sh;chmod +x tools.sh;./tools.sh
-rm tools.sh
 clear
-yellow "MASUKAN DOMAIN ANDA "
+yellow "SEBELUM MASKUIN DOMAIN WAJIB POINTING TERLEBIH DAHULU !!! "
 echo " "
 read -rp "Input ur domain : " -e pp
     if [ -z $pp ]; then
@@ -138,48 +166,172 @@ read -rp "Input ur domain : " -e pp
         echo "IP=$pp" > /var/lib/SIJA/ipvps.conf
     fi
     
-#install ssh ovpn
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green      Install SSH / WS by Hanskor   $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 1
+#install arzsource
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[41;1;39m           >>> Install Tools <<<        \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 5
+wget -q https://raw.githubusercontent.com/Deryko/scvip/main/tools.sh && chmod +x tools.sh && ./tools.sh
+rm tools.sh
+#install ssh-vpn
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[41;1;39m          >>> Install SSH WS <<<        \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 5
 clear
 wget https://raw.githubusercontent.com/Deryko/scvip/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-#Instal Xray
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green      Install XRAY by Hanskor       $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 1
+#install ins-xray
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[41;1;39m            >>> Install Xray <<<         \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 5
 clear
 wget https://raw.githubusercontent.com/Deryko/scvip/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 wget https://raw.githubusercontent.com/Deryko/scvip/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
-#pasang rc clone ssh ovpn 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" 
-echo -e "$green     Memasang Backup by Hanskor     $NC" 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" 
-sleep 2 
+#install br
+echo -e "┌─────────────────────────────────────────┐"
+echo -e " \E[41;1;39m            >>> Install BR <<<           \E[0m$NC"
+echo -e "└─────────────────────────────────────────┘"
+sleep 5 
 clear 
 wget https://raw.githubusercontent.com/Deryko/v4/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 clear
-#Instal slowdns
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green     Install Slowdns by Hanskor     $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-clear
-wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
-clear
+# // Download Data
+echo -e "${GREEN}Download Data${NC}"
+wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/Deryko/scvip/main/menu/clearlog.sh"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu.sh"
+wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-vmess.sh"
+wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-vless.sh"
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Deryko/scvip/main/menu/running.sh"
+wget -q -O /usr/bin/clearcache "https://raw.githubusercontent.com/Deryko/scvip/main/menu/clearcache.sh"
+wget -q -O /usr/bin/menu-trgo "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-trgo.sh"
+wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-trojan.sh"
+wget -q -O /usr/bin/menu-trial "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-trial.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-ssh.sh"
+wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/usernew.sh"
+wget -q -O /usr/bin/trial "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/trial.sh"
+wget -q -O /usr/bin/renew "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/renew.sh"
+wget -q -O /usr/bin/hapus "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/hapus.sh"
+wget -q -O /usr/bin/cek "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/cek.sh"
+wget -q -O /usr/bin/member "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/member.sh"
+wget -q -O /usr/bin/delete "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/delete.sh"
+wget -q -O /usr/bin/autokill "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/autokill.sh"
+wget -q -O /usr/bin/ceklim "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/ceklim.sh"
+wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/tendang.sh"
+wget -q -O /usr/bin/user-lock "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/user-lock.sh"
+wget -q -O /usr/bin/user-unlock "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/user-unlock.sh"
+wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-set.sh"
+wget -q -O /usr/bin/menu-domain "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-domain.sh"
+wget -q -O /usr/bin/add-host "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/add-host.sh"
+wget -q -O /usr/bin/port-change "https://raw.githubusercontent.com/Deryko/scvip/main/port/port-change.sh"
+wget -q -O /usr/bin/certv2ray "https://raw.githubusercontent.com/Deryko/scvip/main/xray/certv2ray.sh"
+wget -q -O /usr/bin/menu-webmin "https://raw.githubusercontent.com/Deryko/scvip/main/menu/menu-webmin.sh"
+wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/speedtest_cli.py"
+wget -q -O /usr/bin/about "https://raw.githubusercontent.com/Deryko/scvip/main/menu/about.sh"
+wget -q -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Deryko/scvip/main/menu/auto-reboot.sh"
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Deryko/scvip/main/menu/restart.sh"
+wget -q -O /usr/bin/bw "https://raw.githubusercontent.com/Deryko/scvip/main/menu/bw.sh"
+wget -q -O /usr/bin/genssl "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/genssl.sh"
+wget -q -O /usr/bin/port-ssl "https://raw.githubusercontent.com/Deryko/scvip/main/port/port-ssl.sh"
+wget -q -O /usr/bin/port-ovpn "https://raw.githubusercontent.com/Deryko/scvip/main/port/port-ovpn.sh"
+wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/xp.sh"
+wget -q -O /usr/bin/acs-set "https://raw.githubusercontent.com/Deryko/scvip/main/acs-set.sh"
+wget -q -O /usr/bin/sshws "https://raw.githubusercontent.com/Deryko/scvip/main/ssh/sshws.sh"
+wget -q -O /usr/bin/updatemenu "https://raw.githubusercontent.com/Deryko/scvip/main/updatemenu.sh"
+cd /usr/sbin
+wget -q -O /usr/bin/mesinssh "https://raw.githubusercontent.com/Deryko/scvip/main/limit/mesinssh.sh"
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/Deryko/scvip/main/limit/cek-ssh.sh"
+chmod +x *
+chmod +x /usr/sbin/*
+cd /usr/bin
+wget -q -O /usr/bin/loop "https://raw.githubusercontent.com/Deryko/scvip/main/limit/loop.sh"
+wget -q -O /usr/bin/matikan "https://raw.githubusercontent.com/Deryko/scvip/main/limit/matikan.sh"
+wget -q -O /usr/bin/limit "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit.sh"
+wget -q -O /usr/bin/limit-ip-ssh "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-ip-ssh.sh"
+wget -q -O /usr/bin/limit-ip-trojan "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-ip-trojan.sh"
+wget -q -O /usr/bin/limit-ip-vless "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-ip-vless.sh"
+wget -q -O /usr/bin/limit-ip-vmess "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-ip-vmess.sh"
+wget -q -O /usr/bin/limit-quota-trojan "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-quota-trojan.sh"
+wget -q -O /usr/bin/limit-quota-vmess "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-quota-vmess.sh"
+wget -q -O /usr/bin/limit-quota-vless "https://raw.githubusercontent.com/Deryko/scvip/main/limit/limit-quota-vless.sh"
+wget -q -O /usr/bin/quota "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota.sh"
+wget -q -O /usr/bin/quota-trojan-grpc "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-trojan-grpc.sh"
+wget -q -O /usr/bin/quota-trojan-ws "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-trojan-ws.sh"
+wget -q -O /usr/bin/quota-vmess-grpc "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vmess-grpc.sh"
+wget -q -O /usr/bin/quota-vmess-ws "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vmess-ws.sh"
+wget -q -O /usr/bin/quota-vless-ws "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vless-ws.sh"
+wget -q -O /usr/bin/quota-vless-grpc "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vless-grpc.sh"
+wget -q -O /usr/bin/quota-vmess-orbit "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vmess-ws-orbit.sh"
+wget -q -O /usr/bin/quota-vmess-orbit1 "https://raw.githubusercontent.com/Deryko/scvip/main/limit/quota-vmess-ws-orbit1.sh"
+chmod +x /usr/bin/*
+cd 
+chmod +x /usr/bin/clearlog
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/menu-vmess
+chmod +x /usr/bin/menu-vless
+chmod +x /usr/bin/running
+chmod +x /usr/bin/clearcache
+chmod +x /usr/bin/menu-trgo
+chmod +x /usr/bin/menu-trojan
+chmod +x /usr/bin/updatemenu
+chmod +x /usr/bin/menu-trial
+chmod +x /usr/bin/menu-ssh
+chmod +x /usr/bin/usernew
+chmod +x /usr/bin/trial
+chmod +x /usr/bin/renew
+chmod +x /usr/bin/hapus
+chmod +x /usr/bin/cek
+chmod +x /usr/bin/member
+chmod +x /usr/bin/delete
+chmod +x /usr/bin/autokill
+chmod +x /usr/bin/ceklim
+chmod +x /usr/bin/tendang
+chmod +x /usr/bin/user-lock
+chmod +x /usr/bin/user-unlock
+chmod +x /usr/bin/menu-set
+chmod +x /usr/bin/menu-domain
+chmod +x /usr/bin/add-host
+chmod +x /usr/bin/port-change
+chmod +x /usr/bin/certv2ray
+chmod +x /usr/bin/menu-webmin
+chmod +x /usr/bin/speedtest
+chmod +x /usr/bin/about
+chmod +x /usr/bin/auto-reboot
+chmod +x /usr/bin/restart
+chmod +x /usr/bin/bw
+chmod +x /usr/bin/port-ssl
+chmod +x /usr/bin/port-ovpn
+chmod +x /usr/bin/xp
+chmod +x /usr/bin/acs-set
+chmod +x /usr/bin/sshws
 
-#Instal udp
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green       Install UDP by Hanskor       $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-sleep 2
-clear
-wget https://raw.githubusercontent.com/Bringas-tunnel/v4/main/udp/udp.sh && chmod +x udp.sh && ./udp.sh
-clear
+cat > /etc/cron.d/re_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
 
+cat > /etc/cron.d/xp_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+
+cat > /etc/cron.d/cl_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+2 1 * * * root /usr/bin/clearlog
+END
+
+cat > /home/re_otm <<-END
+7
+END
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
+
+clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
 
@@ -205,6 +357,7 @@ if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
+serverV=$( curl -sS https://raw.githubusercontent.com/Deryko/scvip/main/version  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
@@ -216,7 +369,7 @@ gg="AM"
 fi
 curl -sS ifconfig.me > /etc/myipvps
 echo " "
-echo "=====================-[ Created => HANSKOR ]-===================="
+echo "=====================-[ HANSKOR TUNNEL ]-===================="
 echo ""
 echo "------------------------------------------------------------"
 echo ""
@@ -257,7 +410,7 @@ echo ""
 echo ""
 echo "------------------------------------------------------------"
 echo ""
-echo "===============-[ Script Created By HANSKOR ]-==============="
+echo "===============-[ Script Created By HANSKOR TUNNEL ]-==============="
 echo -e ""
 echo ""
 echo "" | tee -a log-install.txt
