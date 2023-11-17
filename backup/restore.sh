07d06c1c97b2655499e67c046b5efe9ea2fcf067
Deryko <143024690+Deryko@users.noreply.github.com>
2023-11-18 00:17:56 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -28,15 +28,9 @@ cp passwd /etc/
 cp group /etc/
 cp shadow /etc/
 cp gshadow /etc/
-cp chap-secrets /etc/ppp/
-cp passwd1 /etc/ipsec.d/passwd
-cp -r crot /var/lib/
-cp -r sstp /home/
-cp -r xray /etc/
-cp -r trojan-go /etc/
-cp -r shadowsocksr /usr/local/
-cp -r public_html /home/vps/
 cp crontab /etc/
+cp -r xray /etc/
+cp -r public_html /home/vps/
 strt
 rm -rf /root/backup
 rm -f backup.zip
